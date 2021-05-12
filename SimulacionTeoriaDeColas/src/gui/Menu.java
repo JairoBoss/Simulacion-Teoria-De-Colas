795be938f86181c7c0c56921f659eca139ff9dfa
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-05-11 20:54:03 -0500

    Add files via upload

--- SimulacionTeoriaDeColas/src/gui/Menu.java
+++ SimulacionTeoriaDeColas/src/gui/Menu.java
@@ -5,47 +5,53 @@
  */
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
  * @author megabit
  */
-public class Menu extends JPanel{
+public class Menu extends JPanel {
+
     private JButton btnAceptar;
     private JButton btnCancelar;
     
+
     private JLabel lblTasaDellegada;
     private JLabel lblTasaDeServicio;
-    
+
     private JTextField txtTasaDeLlegada;
     private JTextField txtTasaDeServicio;
 
     public Menu() {
         this.btnAceptar = new JButton("Aceptar");
-        this.btnCancelar = new JButton("Cancelar");
+        this.btnCancelar = new JButton("Cancelar");        
         this.lblTasaDellegada = new JLabel("Tasa de llegada: ");
         this.lblTasaDeServicio = new JLabel("Tasa de servicio: ");
         this.txtTasaDeLlegada = new JTextField(15);
         this.txtTasaDeServicio = new JTextField(15);
-        
-        
-        
-        
-        
-        super.add(btnAceptar);
-        super.add(btnCancelar);
+
+        //Matriz
+        GridLayout gridLayout = new GridLayout(2, 3);
+        gridLayout.setHgap(5);
+        gridLayout.setVgap(5);
+        this.setLayout(gridLayout);
+
+        this.setBorder(new EmptyBorder(10, 30, 30, 30));
+
         super.add(lblTasaDellegada);
         super.add(lblTasaDeServicio);
+        super.add(btnAceptar, BorderLayout.WEST);
         super.add(txtTasaDeLlegada);
         super.add(txtTasaDeServicio);
+        super.add(btnCancelar, BorderLayout.EAST);
 
     }
-    
-    
-    
-    
+
 }
