3200d818e60f0244331d13379e0778e0777109d8
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-05-11 23:55:57 -0500

    Create table.sql

---
+++ SimulacionTeoriaDeColas/src/table.sql
@@ -0,0 +1,13 @@
+create table simulacion(
+	noCamion integer,
+	tiempoEntreLlegada  float,
+	tiempoDeLlegada float,
+	tiempoDeinicioServicio float,
+	tiempoDeEspera float,
+	tiempoDeServicio float,
+	tiempoDeTerminacionServicio float,
+	tiempoEnElSistema float,
+	tiempoOcio float
+);
+
+delete from simulacion
